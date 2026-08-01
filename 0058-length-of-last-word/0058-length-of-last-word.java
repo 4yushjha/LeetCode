8d839e2e98ac6e2be821640cd539a8594224cdbd
Ayush Jha <the.ayush184lap@gmail.com>
2026-08-01 08:58:44 +0530

    Remove comments from lengthOfLastWord method

--- 0058-length-of-last-word/0058-length-of-last-word.java
+++ 0058-length-of-last-word/0058-length-of-last-word.java
@@ -1,12 +1,7 @@
 class Solution {
     public int lengthOfLastWord(String s) {
-        // Step 1: Trim trailing spaces
         s = s.trim();
-        
-        // Step 2: Find last space
         int lastSpaceIndex = s.lastIndexOf(' ');
-        
-        // Step 3: Length of last word = total length - last space index - 1
         return s.length() - lastSpaceIndex - 1;
     }
 }
