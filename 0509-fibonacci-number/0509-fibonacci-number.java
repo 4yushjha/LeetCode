ae23d2a7f9e0ce8e8aed1ebfac7450fd8e073eb3
Ayush Jha <the.ayush184lap@gmail.com>
2026-08-18 21:46:41 +0530

    Time: 0 ms (100%), Space: 42 MB (53.08%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.java
+++ 0509-fibonacci-number/0509-fibonacci-number.java
@@ -12,42 +12,21 @@ class Solution {
 
 
 
-        // if (n <= 1) return n;
+        if (n <= 1) return n;
 
-        // int[] dp = new int[n + 1];
-        // dp[0] = 0;
-        // dp[1] = 1;
+        int[] dp = new int[n + 1];
+        dp[0] = 0;
+        dp[1] = 1;
 
-        // for (int i = 2; i <= n; i++) {
-        //     dp[i] = dp[i - 1] + dp[i - 2];
-        // }
+        for (int i = 2; i <= n; i++) {
+            dp[i] = dp[i - 1] + dp[i - 2];
+        }
 
-        // return dp[n];
+        return dp[n];
 
 
 
-                if (n == 0) return 0;
-        int[][] F = {{1, 1}, {1, 0}};
-        power(F, n - 1);
-        return F[0][0];
-    }
-
-    private void multiply(int[][] F, int[][] M) {
-        int x = F[0][0] * M[0][0] + F[0][1] * M[1][0];
-        int y = F[0][0] * M[0][1] + F[0][1] * M[1][1];
-        int z = F[1][0] * M[0][0] + F[1][1] * M[1][0];
-        int w = F[1][0] * M[0][1] + F[1][1] * M[1][1];
-
-        F[0][0] = x; F[0][1] = y;
-        F[1][0] = z; F[1][1] = w;
-    }
-
-    private void power(int[][] F, int n) {
-        if (n == 0 || n == 1) return;
-        int[][] M = {{1, 1}, {1, 0}};
-        power(F, n / 2);
-        multiply(F, F);
-        if (n % 2 != 0) multiply(F, M);
+        
 
 
 
