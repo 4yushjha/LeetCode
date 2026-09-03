4950a551d8d3ee668c64e7fba631f384d08ca0a1
Ayush Jha <the.ayush184lap@gmail.com>
2026-09-03 21:10:45 +0530

    Time: 4 ms (25.26%), Space: 47.1 MB (51.04%) - LeetHub

--- 1380-lucky-numbers-in-a-matrix/1380-lucky-numbers-in-a-matrix.java
+++ 1380-lucky-numbers-in-a-matrix/1380-lucky-numbers-in-a-matrix.java
@@ -3,30 +3,31 @@ class Solution {
         int M = matrix.length, N = matrix[0].length;
 
         // Step 1: Find row minima
-        List<Integer> rowMin = new ArrayList<>();
-        for (int i = 0; i < M; i++) {
-            int rMin = Integer.MAX_VALUE;
-            for (int j = 0; j < N; j++) {
-                rMin = Math.min(rMin, matrix[i][j]);
+        List<Integer> list1=new ArrayList<>();
+        for(int i=0;i<M;i++){
+            int rMin=Integer.MAX_VALUE;
+            for(int j=0;j<N;j++){
+                rMin=Math.min(rMin,matrix[i][j]);
             }
-            rowMin.add(rMin);
+            list1.add(rMin);
         }
 
         // Step 2: Find column maxima
-        List<Integer> colMax = new ArrayList<>();
-        for (int j = 0; j < N; j++) {
-            int cMax = Integer.MIN_VALUE;
-            for (int i = 0; i < M; i++) {
-                cMax = Math.max(cMax, matrix[i][j]);
+       List<Integer> list2=new ArrayList<>();
+       for(int j=0;j<N;j++){
+            int cMax=Integer.MIN_VALUE;
+            for(int i=0;i<M;i++){
+                cMax=Math.max(cMax,matrix[i][j]);
             }
-            colMax.add(cMax);
-        }
+            list2.add(cMax);
+
+       }
 
         // Step 3: Check lucky numbers
         List<Integer> luckyNumbers = new ArrayList<>();
         for (int i = 0; i < M; i++) {
             for (int j = 0; j < N; j++) {
-                if (matrix[i][j] == rowMin.get(i) && matrix[i][j] == colMax.get(j)) {
+                if (matrix[i][j] == list1.get(i) && matrix[i][j] == list2.get(j)) {
                     luckyNumbers.add(matrix[i][j]);
                 }
             }
