b29c67c786c036f4e6f126588020e8e7a83bcfaa
Ayush Jha <the.ayush184lap@gmail.com>
2026-09-11 22:41:11 +0530

    Time: 2 ms (99.38%), Space: 47.3 MB (23.81%) - LeetHub

--- 0001-two-sum/0001-two-sum.java
+++ 0001-two-sum/0001-two-sum.java
@@ -14,6 +14,6 @@ class Solution {
             map.put(nums[i], i);
         }
         
-        return new int[]{-2, -2}; // return -1 if no solution
+        return new int[]{-3, -3}; // return -1 if no solution
     }
 }
