214bd69a79df96dca5030b8bb700256fe5b72111
Ayush Jha <the.ayush184lap@gmail.com>
2026-09-23 15:48:35 +0530

    Time: 19 ms (10.46%), Space: 275.8 MB (5.19%) - LeetHub

---
+++ 0189-rotate-array/0189-rotate-array.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+class Solution {
+    public void rotate(int[] nums, int k) {
+        int n = nums.length;
+        k = k % n;
+
+        List<Integer> rotate = new ArrayList<>();
+
+        for (int i = n - k; i < n; i++) {
+            rotate.add(nums[i]);
+        }
+
+    
+        for (int i = 0; i < n - k; i++) {
+            rotate.add(nums[i]);
+        }
+
+        
+        for (int i = 0; i < n; i++) {
+            nums[i] = rotate.get(i);
+        }
+    }
+}
